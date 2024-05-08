56d31e13669838d01db0160991fdb3e94ab7cc88
Riccardo <riccardozaupa02@gmail.com>
2024-05-08 17:06:31 +0200

    Inserita una prima versione della paginazione

--- nest-js/src/restaurant/restaurant.controller.ts
+++ nest-js/src/restaurant/restaurant.controller.ts
@@ -8,13 +8,17 @@ export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
   @Get('filter')
-  getFilteredRestaurants(@Query() query: { 
-    date?: string,
-    name?: string,
-    city?: string, 
-    cuisine?: string }) 
-    {
-    return this.restaurantService.getFilteredRestaurants(query);
+  getFilteredRestaurants(
+    @Query() query: { 
+      date?: string,
+      name?: string,
+      city?: string, 
+      cuisine?: string 
+    },
+    @Query('currentPage') currentPage: number,
+    @Query('ITEMS_PER_PAGE') ITEMS_PER_PAGE: number
+  ) {
+    return this.restaurantService.getFilteredRestaurants(query, currentPage, ITEMS_PER_PAGE);
   }
 
   @Post()
--- next-js/src/lib/database/restaurant.ts
+++ next-js/src/lib/database/restaurant.ts
@@ -9,7 +9,7 @@ export interface RestaurantFilter {
 }
 
 //Otteniamo i ristoranti filtrati
-export async function getFilteredRestaurants(params: RestaurantFilter): Promise<[]> {
+export async function getFilteredRestaurants(params: RestaurantFilter, currentPage: number, ITEMS_PER_PAGE: number): Promise<[]> {
     //Inseriamo i parametri nella query string
     let filter = [];
     for (const key in params) {
@@ -17,7 +17,7 @@ export async function getFilteredRestaurants(params: RestaurantFilter): Promise<
             filter.push(`${key}=${params[key as keyof RestaurantFilter]}`);
         }
     }
-    const response = await fetch(`${Endpoints.restaurant}filter?${filter.join('&')}`);
+    const response = await fetch(`${Endpoints.restaurant}filter?currentPage=${currentPage}&ITEMS_PER_PAGE=${ITEMS_PER_PAGE}&${filter.join('&')}`);
     if (!response.ok) {
         throw new Error('Error fetching restaurants from the database');
     }
