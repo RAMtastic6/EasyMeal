2c26da7362d91e2ff9dc3ce5cefe3bb3b4d5f20f
Riccardo <riccardozaupa02@gmail.com>
2024-05-11 19:02:04 +0200

    Aggiunti status, data e ora nella lista di prenotazioni admin

--- next-js/src/components/reservations_admin.tsx
+++ next-js/src/components/reservations_admin.tsx
@@ -26,8 +26,7 @@ export default function ReservationsAdmin() {
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Numero persone</th>
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Stato</th>
-            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Giorno</th>
-            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ora</th>
+            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Giorno e ora</th>
             <th className="px-4 py-2"></th>
           </tr>
         </thead>
@@ -36,15 +35,13 @@ export default function ReservationsAdmin() {
             <tr key={reservation.id} className="hover:bg-gray-100">
               <td className="px-4 py-2">{reservation.id}</td>
               <td className="px-4 py-2">{reservation.number_people}</td>
-              <td className="px-4 py-2">{reservation.status}</td>
-              <td className="px-4 py-2">{reservation.day}</td>
-              <td className="px-4 py-2">{reservation.time}</td>
+              <td className="px-4 py-2">{reservation.state}</td>
+              <td className="px-4 py-2">{(new Date(reservation.date)).toLocaleString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric' })}</td>
               <td className="px-4 py-2"><Link href={`/reservations_admin/${reservation.id}/view`} className="text-orange-500 hover:text-orange-700">Visualizza</Link></td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-
   );
 }
