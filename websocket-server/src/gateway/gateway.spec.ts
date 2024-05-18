e899c46b7519fd5fece0fc149db1050f48494d2c
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-05-18 15:54:10 +0200

    Aggiunti degli unit test per il server socket.

--- websocket-server/src/gateway/gateway.spec.ts
+++ websocket-server/src/gateway/gateway.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MyGateway } from './gateway';
 import e from 'express';
 import { emit } from 'process';
+import { on } from 'events';
 
 describe('MyGateway', () => {
   let gateway: MyGateway;
@@ -17,14 +18,60 @@ describe('MyGateway', () => {
   it('should be defined', () => {
     expect(gateway).toBeDefined();
   });
-
+    
   it('should emit message to room', () => {
     const onMessage = jest.fn();
+    const to = jest.fn().mockReturnThis();
     gateway.server = {
-      to: jest.fn().mockReturnThis(),
+      to: to,
       emit: onMessage,
     } as any;
-    gateway.onIncrement({ id_prenotazione: '123', data: { index: 0, quantity: 1 } });
-    expect(onMessage).toHaveBeenCalledWith('onMessage', { index: 0, quantity: 1 });
+    
+    // chiama la funzione
+    gateway.onIncrement({
+      id_prenotazione: '123',
+      data: { index: 0, quantity: 1 },
+    });
+
+    // verifica che il metodo to sia stato chiamato con il parametro '123'
+    expect(to).toHaveBeenCalledWith('123');
+
+    // verifica che la funzione emit del server sia stata chiamata
+    // con i parametri onMessage e data
+    expect(onMessage).toHaveBeenCalledWith('onMessage', {
+      index: 0,
+      quantity: 1,
+    });
+
+  });
+
+  it('should emit ingredient to room', () => {
+    const onIngredient = jest.fn();
+    const to = jest.fn().mockReturnThis();
+    gateway.server = {
+      to: to,
+      emit: onIngredient,
+    } as any;
+    gateway.onIngredient({
+      id_prenotazione: '123',
+      data: {
+        key: 0,
+        index: 0,
+        ingredientIndex: 0,
+        removed: false,
+      },
+    });
+
+    // verifica che il metodo to sia stato chiamato con il parametro '123'
+    expect(to).toHaveBeenCalledWith('123');
+
+    // verifica che la funzione emit del server sia stata chiamata
+    // con i parametri onIngredient e data
+    expect(onIngredient).toHaveBeenCalledWith('onIngredient', {
+      key: 0,
+      index: 0,
+      ingredientIndex: 0,
+      removed: false,
+    });
   });
 });
