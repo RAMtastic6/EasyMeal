08deb7de914c26d1b7d421438cf8174140ff018e
Riccardo <riccardozaupa02@gmail.com>
2024-05-19 17:35:15 +0200

    Cambiati alcuni aspetti grafici della pagina di prenotazioni admin

--- next-js/src/app/admin/reservations_list/page.tsx
+++ next-js/src/app/admin/reservations_list/page.tsx
@@ -4,7 +4,8 @@ export default function ReservationsAdminPage() {
   return (
     <div className="w-full">
     <div className="container mx-auto mt-4 space-y-4">
-      <h1>Consulta le prenotazioni del tuo ristorante</h1>
+      <h1 className="text-3xl font-semibold">Lista prenotazioni</h1>
+      <p className='text-lg text-gray-500'>Qui puoi visualizzare le prenotazioni effettuate dai clienti</p>
       <ReservationsAdmin />
     </div>
   </div>
--- next-js/src/components/reservations_admin.tsx
+++ next-js/src/components/reservations_admin.tsx
@@ -8,6 +8,13 @@ export default function ReservationsAdmin() {
   const restaurantId = 1; // TO DO: get restaurant id from user
   const [reservations, setReservations] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const stateMessage = {
+    'pending': 'In attesa di conferma',
+    'accept': 'Accettata',
+    'reject': 'Rifiutata',
+    'to_pay': 'Da pagare',
+    'completed': 'Completa',
+  }
   useEffect(() => {
     async function fetchReservations() {
       try {
@@ -27,7 +34,10 @@ export default function ReservationsAdmin() {
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+  if (reservations.length === 0) {
+    return <div>Non è stata effettuata nessuna prenotazione</div>;
+  }
+
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full divide-y divide-gray-200 table-auto">
@@ -36,7 +46,8 @@ export default function ReservationsAdmin() {
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Numero partecipanti</th>
             <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Stato</th>
-            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Giorno e ora</th>
+            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Data</th>
+            <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ora</th>
             <th className="px-4 py-2"></th>
           </tr>
         </thead>
@@ -45,13 +56,20 @@ export default function ReservationsAdmin() {
             <tr key={reservation.id} className="hover:bg-gray-100">
               <td className="px-4 py-2">{reservation.id}</td>
               <td className="px-4 py-2">{reservation.number_people}</td>
-              <td className="px-4 py-2">{reservation.state}</td>
-              <td className="px-4 py-2">{(new Date(reservation.date)).toLocaleString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric' })}</td>
+              <td className="px-4 py-2">
+                <span className={`text-${reservation.state === 'pending' ? 'yellow' : reservation.state === 'accept' ? 'green' : reservation.state === 'reject' ? 'red' : reservation.state === 'completed' ? 'blue' : 'purple'}-500 mr-1`}>&#11044;</span>
+                {stateMessage[reservation.state as keyof typeof stateMessage]}
+              </td>
+              <td className="px-4 py-2">{(new Date(reservation.date)).toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })}</td>
+              <td className="px-4 py-2">{(new Date(reservation.date)).toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' })}</td>
               <td className="px-4 py-2"><Link href={`/admin/reservations_list/${reservation.id}/view`} className="text-orange-500 hover:text-orange-700">Visualizza</Link></td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="mt-4">
+        <p className="text-base text-gray-500">Visualizzate {reservations.length} prenotazioni</p>
+      </div>
     </div>
-  );
+  );  
 }
