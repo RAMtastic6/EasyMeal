1fcac34af21b0cbb09c99a4653aea574722c6101
Riccardo <riccardozaupa02@gmail.com>
2024-05-25 18:09:53 +0200

    Nella prenotazione admin  aggiunto controllo se l'admin rientra in quella prenotazione

--- next-js/src/app/admin/reservations_list/[id]/view/page.tsx
+++ next-js/src/app/admin/reservations_list/[id]/view/page.tsx
@@ -1,17 +1,21 @@
 import ReservationDetails from "@/src/components/reservation_admin";
 import { verifySession } from "@/src/lib/dal";
+import { getReservationsByAdminId } from "@/src/lib/database/reservation";
 import { redirect } from "next/navigation";
 
 export default async function ReservationAdmin({ params }: { params: { id: string } }) {
+  const id = parseInt(params.id);
+  const reservations = await getReservationsByAdminId();
   const session = await verifySession();
-  if(session == null || session.role !== 'admin') {
+  // Check if the user is logged in and if the admin is associated with the reservation
+  if(session == null || session.role !== 'admin' || reservations.map((reservation: any) => reservation.id).includes(id) === false) {
     redirect('/login');
   }
   return (
     <div className="w-full">
       <div className="container mx-auto mt-4">
         <a href="/admin/reservations_list" className="bg-orange-500 text-white px-4 py-2 rounded mb-4 hover:bg-orange-600">Torna alla lista</a>
-        <h1 className="text-2xl mt-4">Consulta la prenotazione {params.id}</h1>
+        <h1 className="text-2xl mt-4">Consulta la prenotazione {id}</h1>
         <div className="mt-4 space-y-4">
           <ReservationDetails params={params} />
         </div>
